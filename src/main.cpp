6670482cf63dfe964544c62ed7744699926e7b4f
Dennis Derks <ds.derks97@gmail.com>
2021-09-13 14:11:44 +0200

    Update main.cpp

--- src/main.cpp
+++ src/main.cpp
@@ -5,7 +5,6 @@
 
 #include <Adafruit_Sensor.h>
 #include <Adafruit_BMP280.h>
-
 #define SEALEVELPRESSURE_HPA (1013.25)
 Adafruit_BMP280 bme;
 
@@ -20,22 +19,8 @@ const int Vin = 5;
 const int temperatureInput = A0;
 const float knownTemperatureResistor = 2000;
 
-int temperatureRaw = 0;
 float unknownTemperatureResistor = 0;
 float temperatureValue = 0;
-
-//////////////
-#define coolantsensorDivider 2970   //defines the resistor value that is in series in the voltage divider
-#define coolantsensorPin A0         //defines the analog pin of the input voltage from the voltage divider
-#define NUMSAMPLES 5                //defines the number of samples to be taken for a smooth average
-
-const float steinconstA = 0.00132774106461327;        //steinhart equation constant A, determined from wikipedia equations
-const float steinconstB = 0.000254470874104285;       //steinhart equation constant B, determined from wikipedia equations
-const float steinconstC = 0.000000101216538378909;    //steinhart equation constant C, determined from wikipedia equations
-
-int samples[NUMSAMPLES];                              //variable to store number of samples to be taken
-//////////////
-
 
 // Pressure
 const int pressureInput = A1;
@@ -57,7 +42,7 @@ void setup()
         Serial.println("Could not find a valid BME280 sensor, check wiring!");
     }
 
-    display.setTextSize(2);
+    display.setTextSize(3);
     display.setTextColor(WHITE);
 
     Serial.println("Starting loop!");
@@ -65,49 +50,18 @@ void setup()
 
 void loop()
 {
-    // Temperature
+    // Temperature        
+    unknownTemperatureResistor = knownTemperatureResistor * ((Vin / ((analogRead(temperatureInput) * Vin) / 1024.0)) -1);
+
     // GND- 2K - A0 - 5V
     //  |-\/\/-|-\/\/-|
-    //temperatureRaw = analogRead(temperatureInput);
-    //float voltage = (temperatureRaw / 1023.0) * 5;
-    //unknownTemperatureResistor = knownTemperatureResistor * ((Vin / ((temperatureRaw * Vin) / 1024.0)) -1);
+    // out[] holds the values wanted in degrees C
+    int out[] = { 170, 160, 150, 140, 130, 120, 110, 100, 90, 80, 70, 60, 50, 40, 30, 20, 10 };
 
-    //temperatureValue = -30.5 * log(unknownTemperatureResistor / 1741.);
-    //temperatureValue = 1350 * pow(log(unknownTemperatureResistor / 200), 2); // ^2
-    //temperatureValue = 560 * pow(log(unknownTemperatureResistor / 260), 2); // ^2
-
-///////////
-    uint8_t i;                                          //integer for loop
-    float average;                                      //decimal for average
-    
-    for (i=0; i<NUMSAMPLES; i++) {                      
-        samples[i] = analogRead(coolantsensorPin);        //takes samples at number defined with a short delay between samples
-        delay(10);
-    }
-
-    average = 0;
-  for (i=0; i< NUMSAMPLES; i++) {
-    average += samples[i];                            //adds all number of samples together
-  }
-  average /= NUMSAMPLES;                              //divides by number of samples to output the average
-
-  Serial.print("Average Analog Coolant Reading = ");
-  Serial.println(average);                                        //analog value at analog pin into arduino
-  average = (coolantsensorDivider*average)/(1023-average);        //conversion equation to read resistance from voltage divider
-  Serial.print("Coolant Sensor Resistance = ");
-  Serial.println(average);
-
-  float steinhart;                              //steinhart equation to estimate temperature value at any resistance from curve of thermistor sensor
-  steinhart = log(average);                     //lnR
-  steinhart = pow(steinhart,3);                 //(lnR)^3
-  steinhart *= steinconstC;                     //C*((lnR)^3)
-  steinhart += (steinconstB*(log(average)));    //B*(lnR) + C*((lnR)^3)
-  steinhart += steinconstA;                     //Complete equation, 1/T=A+BlnR+C(lnR)^3
-  steinhart = 1.0/steinhart;                    //Inverse to isolate for T
-  steinhart -= 273.15;                          //Conversion from kelvin to celcius
-
-
-///////////
+    // in[] holds the measured analogRead() values for defined distances
+    // note: the in array should have increasing values
+    int in[]  = {20, 24, 30, 40, 49, 64, 83, 108, 147, 200, 278, 388, 570, 844, 1286, 2031, 3100 };
+    temperatureValue = multiMap(unknownTemperatureResistor, in, out, 17);
 
     // Pressure
     pressureRaw = analogRead(pressureInput);
@@ -115,13 +69,23 @@ void loop()
     pressureValueBar = pressureValue * 0.0689475729;
 
     display.clearDisplay();
-    display.setCursor(0, 0);
+    display.setCursor(5, 0);
 
-    display.print(steinhart);
-    display.println(" *C");
+//    display.print("R:");
+//    display.println(unknownTemperatureResistor);
 
-    display.print(pressureValueBar);
-    display.println(" bar");
+    display.print(temperatureValue, 1);
+    display.setCursor(90, -14);
+    display.print(".");
+    display.setCursor(105, 0);
+    display.print("C");
+
+    display.setCursor(5, 30);
+    display.print(pressureValueBar, 1);
+    display.setCursor(97, 30);
+    display.print("b");
+    display.setCursor(110, 30);
+    display.print(".");
 
 //    display.print(bme.readTemperature());
 //    display.println(" *C");
@@ -137,5 +101,24 @@ void loop()
 
     display.display();
 
-    delay(100);
+    delay(50);
+}
+
+// note: the _in array should have increasing values
+int multiMap(int val, int* _in, int* _out, uint8_t size)
+{
+  // take care the value is within range
+  // val = constrain(val, _in[0], _in[size-1]);
+  if (val <= _in[0]) return _out[0];
+  if (val >= _in[size-1]) return _out[size-1];
+
+  // search right interval
+  uint8_t pos = 1;  // _in[0] allready tested
+  while(val > _in[pos]) pos++;
+
+  // this will handle all exact "points" in the _in array
+  if (val == _in[pos]) return _out[pos];
+
+  // interpolate in the right segment for the rest
+  return (val - _in[pos-1]) * (_out[pos] - _out[pos-1]) / (_in[pos] - _in[pos-1]) + _out[pos-1];
 }
